0335affbc7b94e8640055b76876fc9bc6492cc8d
lerocha34 <le.vieirarocha@gmail.com>
2024-04-05 11:13:33 -0300

    bug fix do botão de start

--- js/script.js
+++ js/script.js
@@ -5,41 +5,43 @@ let start = document.getElementById("start");
 let clear = document.getElementById("clear");
 let pause = document.getElementById("pause");
 
-countSeconds = 0;
-countMinutes = 0;
-countHours = 0;
+let countSeconds = 0;
+let countMinutes = 0;
+let countHours = 0;
 let intervalId;
-let counting = true;
+let counting = false;
 
 function startCount() {
-  if (counting) {
-    function startSeconds(valor) {
-      countSeconds = countSeconds + valor;
-      seconds.innerHTML = countSeconds;
-      if (countSeconds > 59) {
-        countMinutes = countMinutes + valor;
-        minutes.innerHTML = countMinutes;
-        seconds.innerHTML = 0;
-        countSeconds = 0;
-      }
-      if (countMinutes > 59) {
-        countHours = countHours + valor;
-        hours.innerHTML = countHours;
-        minutes.innerHTML = 0;
-        countMinutes = 0;
-      }
+  if (!counting) {
+    counting = true;
+    intervalId = setInterval(startSeconds, 1000);
+  }
+}
+
+function startSeconds() {
+  countSeconds++;
+  seconds.innerHTML = countSeconds;
+  if (countSeconds > 59) {
+    countMinutes++;
+    minutes.innerHTML = countMinutes;
+    seconds.innerHTML = 0;
+    countSeconds = 0;
+    if (countMinutes > 59) {
+      countHours++;
+      hours.innerHTML = countHours;
+      minutes.innerHTML = 0;
+      countMinutes = 0;
     }
   }
-  intervalId = setInterval(startSeconds, 1000, 1);
-
-  return (counting = false);
 }
+
 function pauseTime() {
-  if (counting == false) {
+  if (counting) {
     clearInterval(intervalId);
-    return (counting = true);
+    counting = false;
   }
 }
+
 function clearTime() {
   clearInterval(intervalId);
   countSeconds = 0;
@@ -49,7 +51,9 @@ function clearTime() {
   seconds.innerHTML = 0;
   minutes.innerHTML = 0;
   hours.innerHTML = 0;
+  counting = false; // Certifique-se de marcar como não contando
 }
+
 start.addEventListener("click", startCount);
 pause.addEventListener("click", pauseTime);
 clear.addEventListener("click", clearTime);
